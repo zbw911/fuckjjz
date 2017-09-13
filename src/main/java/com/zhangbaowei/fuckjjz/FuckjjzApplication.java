a38095cd860214476d0267cc994879269170516c
zhangbaowei <zbw911@gmail.com>
2017-09-13 10:35:40 +0800

    init

---
+++ src/main/java/com/zhangbaowei/fuckjjz/FuckjjzApplication.java
@@ -0,0 +1,12 @@
+package com.zhangbaowei.fuckjjz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuckjjzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuckjjzApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zhangbaowei/fuckjjz/FuckjjzApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhangbaowei.fuckjjz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FuckjjzApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
